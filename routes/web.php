7c2c29939fc2e5946f0e7b0dc8ec72d8b995f940
maveriksEcho <maveriks.echo@gmail.com>
2018-06-14 20:58:47 +0300

    14/06/18

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('/', 'DashboardController@index')->name('admin.dashboard');
+});
